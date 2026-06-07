5e97c21f884e9d5390c35819b9656140a3c8b694
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2026-06-07 16:01:04 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0],profit=0;
+        for(int i:prices){
+            buy = min(buy,i);
+            profit = max(profit,i-buy);
+        }
+        return profit;
+    }
+};
