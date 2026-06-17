6c5d9ef0b463cd166098f3789514e4004861416a
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2026-06-17 18:31:45 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int ans = 0;
+        for(int i=0; i<=31; i++){
+            if((n>>i)&1){
+                ans|=1<<(31-i);
+            }
+        }
+        return ans;
+    }
+};
